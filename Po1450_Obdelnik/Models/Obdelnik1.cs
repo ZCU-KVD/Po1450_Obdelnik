f9f3559ccca6e38b275662a922cfb82e23869b08
Zdenek ULRYCH <ulrychz@office365.zcu.cz>
2025-02-17 16:27:23 +0100

    Přidejte soubory projektu.

---
+++ Po1450_Obdelnik/Models/Obdelnik1.cs
@@ -0,0 +1,31 @@
+﻿namespace Po1450_Obdelnik.Models
+{
+	public class Obdelnik1
+	{
+		public int StranaA { get; set; }
+		public int StranaB { get; set; }
+
+		public int Obvod;
+		public int Obsah;
+		public double Uhlopricka;
+		private void VypocetObvod()
+		{
+			Obvod = 2 * (StranaA + StranaB);
+		}
+		private void VypocetObsah()
+		{
+			Obsah = StranaA * StranaB;
+		}
+		private void VypocetUhlopricka()
+		{
+			Uhlopricka = Math.Sqrt(StranaA * StranaA + StranaB * StranaB);
+		}
+		public void Vypocet()
+		{
+			VypocetObsah();
+			VypocetObvod();
+			VypocetUhlopricka();
+		}
+
+	}
+}
---
+++ Po1450_Obdelnik/Models/Obdelnik2.cs
@@ -0,0 +1,13 @@
+﻿namespace Po1450_Obdelnik.Models
+{
+	public class Obdelnik2
+	{
+		public int StranaA { get; set; }
+		public int StranaB { get; set; }
+
+		public int Obvod => 2 * (StranaA + StranaB);
+		public int Obsah => StranaA * StranaB;
+		public double Uhlopricka => Math.Sqrt(StranaA * StranaA + StranaB * StranaB);
+
+	}
+}
